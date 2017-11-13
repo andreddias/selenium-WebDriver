e42f6ec3507e42d68b55e61a8d136674220b99b0
Andre Damasceno Dias <andredamascenodias@gmail.com>
2017-11-13 14:17:06 -0200

    Create UtilMap.java

---
+++ UtilMap.java
@@ -0,0 +1,16 @@
+package system;
+
+public interface UtilMap {
+	
+	//Url
+	String URL = "http://www.site.com.br/";
+	
+	//Login
+	String MENU = "//*[@id='pageTop']/div/div/ul/li[2]/a";
+	String BTNLOGIN = "html/body/div[2]/div[4]/div[1]/div/ul/fieldset[1]/li[1]/a/img";
+	String FIELDLOGIN = "//*[@id='forms']/fieldset/div/form/div[1]/div[1]/span[1]/span[2]/input";
+	String FIELDPASSWORD = "//*[@id='forms']/fieldset/div/form/div[1]/div[1]/span[2]/span[2]/input";
+	String BTNENTER = "//*[@id='forms']/fieldset/div/form/div[1]/input";
+	String MENU2 = "html/body/table/tbody/tr[1]/td/div/div/div/ul/li[1]/a";
+
+}
